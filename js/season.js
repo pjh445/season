9bb5165700de7ab010f3fb9397d4b1224eb0c36f
jeonghwa,park <pjh445@gmail.com>
2023-08-26 21:42:29 +0900

    Add files via upload

--- js/season.js
+++ js/season.js
@@ -1,39 +1,9 @@
-$(document).ready(function(){
-			let act;
-			let aa;
-			//aa = $(this).window().location.href;
-			console.log(this.location.href);
-			//$(location).attr('href',url);
-			
+$(document).ready(function(){			
 			let href = this.location.href;
-			if(href.substr(-1) == 0) {
-					$("nav li").eq(0).children().addClass("active");
-			}
+			let i = href.substr(-1);
+			("nav a").removeClass("active");
+			$("nav li").eq( i ).children().addClass("active");
+						
 			
-			$("nav li").eq(act).children().addClass("active");
-			
-			
-			let address = location.hef = "spring.html?act=" + act;	
-			$("nav a").click(function(act){
-				console.log( act );
-				if( act  == 0 ){						
-						$("nav li").eq(0).children().addClass("active");						
-				}
-			});
-				
-				/*				
-				let i = $(this).parent().index();
-				let season;
-				let act;
-				switch(i){
-					case 0 : season = "spring.html?act=" + 0; break;
-					case 0 : season = "spring.html?act=" + 1; break;
-					case 0 : season = "spring.html?act=" + 2; break;
-					case 0 : season = "spring.html?act=" + 3;
-				}
-				location.hef = season;	
-				$("nav li a").removeClass("active");		
-			*/
-			//location.href="/경로?보낼변수명=" + 값;
 
 });//////전체 끝
